2f6a5594a23f32090e100717ba5c7b997421bb8c
1mag1n33 <kopeccarson@gmail.com>
2025-02-18 19:33:10 -0500

    Initial commit

---
+++ Common/src/main/java/com/github/ImagineForge/Radion/Constants.java
@@ -0,0 +1,11 @@
+package com.github.ImagineForge.Radion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "radion";
+	public static final String MOD_NAME = "Radion";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
