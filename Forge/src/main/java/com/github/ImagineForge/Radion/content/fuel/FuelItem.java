3988f590d2207cc02a4137f2ec4f5c51b5f3baaa
CyberWordPL <cyberwordixx@gazeta.pl>
2025-02-28 21:40:59 +0100

    Add FuelItem

--- Forge/src/main/java/com/github/ImagineForge/Radion/content/fuel/FuelItem.java
+++ Forge/src/main/java/com/github/ImagineForge/Radion/content/fuel/FuelItem.java
@@ -1,4 +1,18 @@
 package com.github.ImagineForge.Radion.content.fuel;
 
-public class FuelItem {
+import net.minecraft.world.item.Item;
+
+public abstract class FuelItem extends Item {
+    private int usableFuel;
+
+    public FuelItem(Properties properties) {
+        super(properties);
+    }
+
+    void removeUsableFuel(int amount) {
+        usableFuel -= amount;
+        if (usableFuel <= 0) {
+            usableFuel = 0;
+        }
+    }
 }
