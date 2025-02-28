5c7b4772fb2905491576a2e9e1b1894f6962f7bf
RealCatDev <real.catdev@gmail.com>
2025-02-28 19:32:08 +0100

    Fixed Imagine's skill issue

--- Forge/src/main/java/com/github/ImagineForge/Radion/registry/CreativeTabs.java
+++ Forge/src/main/java/com/github/ImagineForge/Radion/registry/CreativeTabs.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class CreativeTabs {
-    public static final CreativeModeTab RADION_TAB = new CreativeModeTab(1, "radion_tab") {
+    public static final CreativeModeTab RADION_TAB = new CreativeModeTab("radion_tab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(items.URANIUM_INGOT.get().getDefaultInstance().getItem());
